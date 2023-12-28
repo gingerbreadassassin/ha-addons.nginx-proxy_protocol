72257a2bd1810ccb1b300b10d05a69ce23a8c76f
Connor Ness <connordvn@gmail.com>
2023-12-28 20:28:12 +0000

    Prepare for github

--- openthread_border_router/rootfs/etc/s6-overlay/scripts/otbr-agent-configure.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Configure OTBR depending on add-on settings
-# ==============================================================================
-
-if bashio::config.true 'nat64'; then
-    bashio::log.info "Enabling NAT64."
-    ot-ctl nat64 enable
-fi
